8dda82abf3ee6f1cdb56e94e02bbc7cc72dec0c3
pc <pc@LAPTOP-D93TPG0A>
2018-08-15 12:35:11 +0700

    Initial Commit.

--- src/jobsheet11/latihan2.java
+++ src/jobsheet11/latihan2.java
@@ -7,56 +7,9 @@ public class latihan2 {private static void tampilJudul(String identitas)
     
     System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
 }
-   
 
 public static void main(String[] args){
     String identitas = "az zahra dita pratiwi / XRPL3 / 9 ";
     tampilJudul(identitas);
-    String kalimat = tampilInput();
-    String convert = vokal2Angka(kalimat);
-    tampilPerKata(kalimat, convert);
-    tampilHasil(convert);
 }
-    private static String tampilInput()
-    {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Masukkan Kalimat: ");
-        String kalimat = scanner.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
-        
-        return kalimat;
-    }   
-        
-        
-        public static String vokal2Angka(String kalimat)
-        {
-            char [][] arConvert =
-            {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-            
-            kalimat = kalimat.toLowerCase();
-            for (int i = 0; i < arConvert.length; i++) 
-                kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-            
-            return kalimat;
-        }    
-            private static void tampilPerKata(String kalimat, String convert)
-            {
-                String[] arrKal = kalimat.split(" ");
-                String[] arrCon = convert.split(" ");
-                
-                for (int i = 0; i < arrKal.length; i++) 
-                    System.out.println(arrKal[i]+" => "+ arrCon[i]);
-            }
-            
-            
-            private static void tampilHasil(String convert)
-            {
-                System.out.println("Kalimat Alay Angka : " + convert);
-            }
-            
-            
-            
-        }
-    
-
+}
